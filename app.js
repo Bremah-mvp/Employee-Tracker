a5e3b6bc6c0ff8c46068745724c89125620d17a8
jackie <mvpbremah@yahoo.com>
2020-09-11 21:52:01 -0400

    commit

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-const EmpData = require('')
+const EmployeesD = require('./lib/EmployeesD');
+const connection = require('./lib/connDB');
+const cTable = require('console.table');
+const figlet = require('figlet');
+const inquirer = require('inquirer');
+const view = require('/lib/viewFunction');
+const add = require("./lib/functions");
+const del = require('./lib/delFunc');
+const update = require('./lib/updFuncs');
+
+// use inquire to prompt user for information
+const 
