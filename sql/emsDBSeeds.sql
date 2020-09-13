a607e70a1da0304415a94d71a4853d9104736f3f
jackie <mvpbremah@yahoo.com>
2020-09-13 00:01:53 -0400

    commit

--- sql/emsDBSeeds.sql
+++ sql/emsDBSeeds.sql
@@ -1,9 +1,8 @@
-INSERT INTO department (name)
-VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
+INSERT INTO employee (first_name, last_name, role_id, manager)
+VALUES ("Bremah", "Bremen", 3, NULL), ("Zoey", "Trinity", 4, 1), ("Frankie", "Semp", 6, NULL), ("Babie", "Suzie", 2, NULL), ("Jackie", "Nanyo", 1, 4);
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Salesperson", 80000, 1), ("Sales Lead", 100000, 1), ("Lead Engineer", 150000, 2), ("Software Engineer", 120000, 2), ("Accountant", 130000, 3), ("Lawyer", 190000, 4), ("Legal Team Lead", 250000, 4);
+VALUES ("Salesperson", 40000, 1), ("Sales Head", 70000, 1), ("Head Information Technology", 120000, 2), ("IT Specialist", 100000, 2), ("Lawyer", 150000, 3), ("Accountant", 160000, 4), ("Human Resource GM", 180000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jon", "Pertwee", 3, NULL), ("David", "Tennant", 4, 1), ("Lis", "Sladen", 6, NULL), ("Sarah", "Sutton", 2, NULL), ("Peter", "Capaldi", 1, 4);
-
+INSERT INTO department (name)
+VALUES ("Sales"),  ("Information Technology"), ("Finance"), ("Legal");
